6c3c8d47a2c022a9ffa6ec0060264c7917dda77d
Jakub Krhovjak <jakub.krhovjak@morosystems.cz>
2019-03-25 07:04:05 +0100

    Two way messaging

--- order-service/src/main/java/net/japca/order/OrderService.java
+++ order-service/src/main/java/net/japca/order/OrderService.java
@@ -11,7 +11,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
@@ -19,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@RestController
 @EnableBinding({OrderProcessor.class})
 public class OrderService {
 
@@ -30,13 +36,24 @@ public class OrderService {
     private OrderProcessor orderProcessor;
 
 
+    @GetMapping("/")
+    public String sendOrder() {
+        orderProcessor.orderOut().send((MessageBuilder
+                .withPayload(new Order("Order test", 3))
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build()));
+
+        return "ok";
+    }
+
+
     @StreamListener(OrderProcessor.ORDER_UPDATED)
     public void receiveUpdatedOrder(Order order) {
         log.info("Updated order received: {}", order);
     }
 
     @Bean
-    @InboundChannelAdapter(value = OrderProcessor.ORDER_OUT, poller = @Poller(fixedRate = "3000", maxMessagesPerPoll = "1"))
+    @InboundChannelAdapter(value = OrderProcessor.ORDER_OUT, poller = @Poller(fixedRate = "1000", maxMessagesPerPoll = "1"))
     public MessageSource<Order> ordersSource() {
         Random random = new Random();
         return () -> new GenericMessage<>(new Order("new order " +  random.nextInt(100), random.nextInt(10)));
--- shop-service/src/main/java/net/japca/shop/ShopService.java
+++ shop-service/src/main/java/net/japca/shop/ShopService.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,12 +27,15 @@ public class ShopService {
 	public Order receiverOrder(Order order) {
 		log.info("order received: {}", order);
 		order.setDescription(" Description changed");
+		if(order.getCount() == 3) {
+			throw new RuntimeException("Exception");
+		}
 		return order;
 	}
 
-//	@ServiceActivator(inputChannel = OrderProcessor.ORDER_SEND + ".shop-service.errors")
-//	public void error(Order message) {
-//		System.out.println("Handling ERROR: " + message);
-//	}
+	@ServiceActivator(inputChannel = OrderProcessor.ORDER_SEND + ".shop-service.errors")
+	public void error(Message<?> message) {
+		log.error("Handling ERROR: " + message);
+	}
 
 }
